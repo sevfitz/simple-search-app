001eaa5bf21056bbe6c78be315a735efb7435b52
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-11-29 23:20:42 -0800

    Add Results component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Jumbotron, Grid } from 'react-bootstrap';
 import SearchForm from './components/SearchForm';
+import Results from './components/Results';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
             <h1>Search App</h1>
             <p>This is a simple search app based on the Treehouse Create-React-App course example</p>
             <SearchForm />
+            <Results />
           </Grid>
         </Jumbotron>
       </div>
