c2cdf11f3a82ff3a0b388741500903ef88ba1188
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-11-29 22:57:34 -0800

    Create SearchForm component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { Jumbotron, Grid } from 'react-bootstrap';
+import SearchForm from './components/SearchForm';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
+      <div>
+        <Jumbotron>
+          <Grid>
+            <h1>Search App</h1>
+            <p>This is a simple search app based on the Treehouse Create-React-App course example</p>
+            <SearchForm />
+          </Grid>
+        </Jumbotron>
       </div>
     );
   }
